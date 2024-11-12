975953c1dbe04823e65411dfa4699adb90c6074a
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-11-12 16:44:18 +0100

    refractoring and adding head and tail methods

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 def main():
+   location = 'data/df_test.csv'
    cols =  ["price", "bedrooms", "living_in_m2", "renovated"]
-   df_minimize = read(cols)
-   print(df_minimize)
+   df = read(location,cols)
+
+   #seeing the first and last 5 of datast
+   print(df.head())
+   print(df.tail())
 
 
-def read(cols):
-    df = pd.read_csv('data/df_test.csv', usecols=cols)
+def read(location,cols):
+    df = pd.read_csv(location, usecols=cols)
     return df
 
 

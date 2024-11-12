cb1461eeaa0eb392d0012db196d1f04a9142d600
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-11-12 13:41:47 +0100

    refractoring read method

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 def main():
+   cols =  ["price", "bedrooms", "living_in_m2", "renovated"]
+   df_minimize = read(cols)
+   print(df_minimize)
 
 
-    df = pd.read_csv('data/df_test.csv')
-    df_minimize = pd.read_csv('data/df_test.csv', usecols=[ "price", "bedrooms", "living_in_m2", "renovated"])
-    print(df_minimize)
+def read(cols):
+    df = pd.read_csv('data/df_test.csv', usecols=cols)
+    return df
+
 
 if __name__ == "__main__":
     main()
